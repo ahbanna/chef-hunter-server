7c2f83029ffa9bbe730ec2d16e597bf01ddbd964
Al Hasanul Banna <tutul5800@gmail.com>
2023-05-02 21:01:02 +0600

    create shef json data

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const chef = require("./data/chef.json");
+
 app.get("/", (req, res) => {
   res.send("serevr running");
+});
+app.get("/chef", (req, res) => {
+  res.send(chef);
 });
 app.listen(port, () => {
   console.log("2nd running");
